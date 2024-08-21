350caf91a2d6d10ad6c84d0e0a6583cd93fd3e72
Insanekun <yamero.yugen@gmail.com>
2024-08-21 22:33:33 +0530

    Final Deploy

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const jsonServer = require("json-server"); // importing json-server library
-const server = jsonServer.create();
-const router = jsonServer.router("blog-data.json");
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
+const jsonServer = require("json-server"); // Importing json-server library
+const cors = require("cors"); // Importing cors library
 
+const server = jsonServer.create();
+const router = jsonServer.router("blog-data.json"); // Replace with your JSON file
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8000; // Choose port from here like 8080, 3001
+
+server.use(cors()); // Enable CORS for all routes
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
